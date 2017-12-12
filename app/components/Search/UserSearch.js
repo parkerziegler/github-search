e84f278c18178b0f37bff5a9e914a2ee20665dc6
parkerziegler <parkerellisziegler@gmail.com>
2017-12-11 18:04:48 -0800

    Making UserSearch more concrete as a component.

--- app/components/Search/UserSearch.js
+++ app/components/Search/UserSearch.js
@@ -1,20 +1,39 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { TextInput, StyleSheet } from 'react-native';
+import { trackUserSearch } from '../../actions/searchActions';
 
-export default class UserSearch extends Component {
+class UserSearch extends Component {
+
+    onChangeText = searchInput => {
+
+        const { dispatch } = this.props;
+
+        dispatch(trackUserSearch(searchInput));
+    }
 
     render() {
-        const { onChangeText, searchInput } = this.props;
+
+        const { search } = this.props;
 
         return (
             <TextInput
                 style={styles.input}
-                value={searchInput}
-                onChangeText={text => onChangeText(text)}
+                value={search.searchInput}
+                onChangeText={this.onChangeText}
                 placeholder="Search for a GitHub user" />
         );
     }
 }
+
+const mapStateToProps = (state) => {
+
+    return {
+        search: state.searchReducer
+    };
+};
+
+export default connect(mapStateToProps)(UserSearch);
 
 const styles = StyleSheet.create({
     input: {
