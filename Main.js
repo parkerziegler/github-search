9b9cfc87b8a9dd832a646ec623a0b9a94103453f
parkerziegler <parkerellisziegler@gmail.com>
2017-12-20 17:26:32 -0800

    Rewriting Main to pass off data loading to Apollo GraphQL.

--- Main.js
+++ Main.js
@@ -8,79 +8,57 @@ import { flagLoading, getRepos, getAvatar } from './app/actions/searchActions';
 
 class Main extends React.Component {
 
+    state = {
+        showResults: false
+    }
+
     onSubmitHandler = () => {
 
-        // destructure props
-        const { dispatch, search } = this.props;
+        this.setState({
+            showResults: true
+        }); 
+    }
 
-        // flag state as loading
-        dispatch(flagLoading(true));
+    onBackHandler = () => {
 
-        // format our GraphQL query
-        const query = `query {
-            repositoryOwner(login: "${search.searchInput}") {
-            url,
-            avatarUrl(size: 100),
-            repositories(first: 5) {
-                nodes {
-                name,
-                description
-                }
-            }
-            }
-        }`;
-
-        /* make the request to the GitHub GraphQL API. we'll store
-        the repos and user avatar in redux. */
-        fetch('https://api.github.com/graphql', {
-            method: 'POST',
-            body: JSON.stringify({ query }),
-            headers: { 'Authorization': 'bearer ' + token }
-        })
-            .then(res => res.json())
-            .then(res => {
-
-                console.log(res);
-                
-                // send the repos and avatar to redux
-                dispatch(getRepos(res.data.repositoryOwner.repositories.nodes));
-                dispatch(getAvatar(res.data.repositoryOwner.avatarUrl));
-                
-                // mark the state as done loading
-                dispatch(flagLoading(false));
-            })
-            .catch(err => console.log(err));
+        this.setState({
+            showResults: false
+        });
     }
 
     render() {
 
-    // destructure props
-   //  const { loading, repos, searchInput, avatar } = this.props.search;
+        // destructure props and state
+        const { searchInput } = this.props.search;
+        const { showResults } = this.state;
 
-    return (
-        <View style={styles.container}>
-            {/*<View style={styles.statusBarBackground}></View>
-            <View style={styles.titleContainer}>
-            <Image
-                style={{ width: 100, height: 40 }}
-                source={require('./assets/GitHub_Logo.png')} />
-            <Text style={styles.title}>Search</Text>
-            </View>
-            {!repos.length
-            ? <Image
-                style={{ width: 100, height: 100, marginBottom: 20 }}
-                source={require('./assets/GitHub-Mark-120px-plus.png')}/>
-            : null}
-            <UserSearch />
-            <Button title="Search" onPress={this.onSubmitHandler}/>
-            {!loading && avatar
-            ? <Image
-                style={{width: 50, height: 50}}
-                source={{ uri: avatar }}/>
-            : null}*/}
-            <RepositoryList />
-        </View>
-    );
+        const search = <View style={styles.container}>
+                <View style={styles.statusBarBackground}></View>
+                <View style={styles.titleContainer}>
+                    <Image
+                        style={{ width: 100, height: 40 }}
+                        source={require('./assets/GitHub_Logo.png')} />
+                    <Text style={styles.title}>Search</Text>
+                </View>
+                <Image
+                    style={{ width: 100, height: 100, marginBottom: 20 }}
+                    source={require('./assets/GitHub-Mark-120px-plus.png')}/>
+                <UserSearch />
+                <Button title="Search" onPress={this.onSubmitHandler}/>
+                {/*{!loading && avatar
+                ? <Image
+                    style={{width: 50, height: 50}}
+                    source={{ uri: avatar }}/>
+                : null}}*/}
+            </View>;
+
+        const results = <View style={styles.container}>
+                <View style={styles.statusBarBackground}></View>
+                <RepositoryList />
+                <Button title="Back" onPress={this.onBackHandler}/>
+            </View>;
+
+        return showResults ? results : search;
     }
 }
 
