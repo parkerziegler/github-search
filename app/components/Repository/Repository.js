2bf6db3618478fe1342b9ec660078f4a42ff2b9d
parkerziegler <parkerellisziegler@gmail.com>
2017-12-27 20:24:55 -0800

    Updating styling on Repository component.

--- app/components/Repository/Repository.js
+++ app/components/Repository/Repository.js
@@ -18,7 +18,7 @@ export default class Repository extends React.Component {
 
         return (
             <View style={styles.repoContainer}>
-                <Button title={name} iconRight={{name: "call-made", color: "#000"}} small onPress={this.handleTitlePress} backgroundColor="#A1A1A1" textStyle={styles.repoName} buttonStyle={{alignSelf: 'flex-start'}} />
+                <Button title={name} iconRight={{name: "call-made", color: "#000"}} small onPress={this.handleTitlePress} textStyle={styles.repoName} backgroundColor="#FFF" buttonStyle={{alignSelf: 'flex-start', paddingBottom: 0.5, paddingLeft: 0}} containerViewStyle={{borderBottomColor: '#000', borderBottomWidth: 1, marginLeft: 0, marginRight: 0}}/>
                 <Text style={styles.repoDescription}>{description}</Text>
             </View>
         );
@@ -29,12 +29,12 @@ const styles = StyleSheet.create({
     repoContainer: {
         display: 'flex',
         flex: 1,
-        backgroundColor: 'red'
+        marginBottom: 5
     },
     repoName: {
         color: '#000000',
         fontSize: 20,
-        fontFamily: (Platform.OS === 'ios') ? "Avenir Next" : "Roboto",
+        fontFamily: (Platform.OS === 'ios') ? "HelveticaNeue" : "Roboto",
         fontWeight: (Platform.OS === 'ios') ? "500" : "700"
     },
     repoDescription: {
