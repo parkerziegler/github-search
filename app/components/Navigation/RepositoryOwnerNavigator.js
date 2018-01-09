107194e634046d753f838ee20631c8bcc4e8040c
parkerziegler <parkerellisziegler@gmail.com>
2018-01-09 14:16:18 -0800

    Making whole app run on a Stack Navigator.

--- app/components/Navigation/RepositoryOwnerNavigator.js
+++ app/components/Navigation/RepositoryOwnerNavigator.js
@@ -1,21 +1,47 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+import SearchScreen from '../Screens/SearchScreen';
 import RepositoryScreen from '../Screens/RepositoryScreen';
 import AuthorScreen from '../Screens/AuthorScreen';
+import { Button } from 'react-native-elements';
+import { Constants } from 'expo';
 
 const RepositoryOwnerNavigator = StackNavigator({
+    SearchScreen: {
+        screen: SearchScreen,
+        navigationOptions: ({ navigation }) => ({
+            header: null,
+        })
+    },
     RepositoryScreen: {
         screen: RepositoryScreen,
-        navigationOptions: ({navigation}) => ({
-            title: 'Repositories',
-            header: null
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: { marginTop: Constants.statusBarHeight },
+            title: "Search"
         })
     },
     AuthorScreen: {
-        screen: AuthorScreen
+        screen: AuthorScreen,
+        navigationOptions: ({ navigation }) => ({
+            headerStyle: { marginTop: Constants.statusBarHeight },
+            title: "Repositories"
+        })
     }
 }, {
-    initialRouteName: "RepositoryScreen"
+    initialRouteName: "SearchScreen"
+});
+
+const styles = StyleSheet.create({
+    button: {
+        marginTop: 10,
+        paddingLeft: 20,
+        paddingRight: 20
+    },
+    header: {
+        borderColor: 'red',
+        borderWidth: 1
+    }
 });
 
 export default RepositoryOwnerNavigator;
