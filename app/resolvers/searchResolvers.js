4e515c895ee612b0f72d410774d5e0e26b6b415a
parkerziegler <parkerellisziegler@gmail.com>
2018-01-10 16:51:01 -0800

    Adding resolvers for apollo-link-state.

---
+++ app/resolvers/searchResolvers.js
@@ -0,0 +1,21 @@
+const searchResolvers = {
+    defaults: {
+        __typename: "SearchInput",
+        search: ''
+    },
+    resolvers: {
+      Mutation: {
+        trackSearch: (_, search, { cache }) => {
+          
+          const data = {
+            __typename: "SearchInput",
+            search
+          };
+          
+          cache.writeData({ data });
+        }
+      }
+    }
+};
+
+export default searchResolvers;
