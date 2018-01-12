864860c474c10a581128f49ef990150e17720ebf
parkerziegler <parkerellisziegler@gmail.com>
2018-01-11 20:46:37 -0800

    Proper update pattern for resolvers.

--- app/resolvers/searchResolvers.js
+++ app/resolvers/searchResolvers.js
@@ -1,18 +1,30 @@
+import getSearch from '../graphql/getSearch';
+
 const searchResolvers = {
     defaults: {
-        __typename: "SearchInput",
-        search: ''
+        search: {
+            input: "",
+            __typename: "SearchInput",
+        }
     },
     resolvers: {
       Mutation: {
-        trackSearch: (_, { search }, { cache }) => {
+        trackSearch: (_, { input }, { cache }) => {
+
+            console.log("Input in resovler " + input);
+            const query = getSearch;
+
+            const previousState = cache.readQuery({ query });
+
+            const data = {
+                ...previousState,
+                search: {
+                    input
+                }
+            };
+
+            cache.writeData({ query, data });
           
-          const data = {
-            __typename: "SearchInput",
-            search
-          };
-          
-          cache.writeData({ data });
         }
       }
     }
